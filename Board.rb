8684af34db21164c095cd322cdb5b3f4ab85b797
Timothy Chang <tchang135@gmail.com>
2022-11-29 17:16:07 -0500

    finished move piece

--- Board.rb
+++ Board.rb
@@ -4,22 +4,25 @@ class Board
   attr_accessor :grid
 
   def initialize
-    @grid = Array.new(8) { Array.new(8, nil) }
+    @grid = Array.new(8) { Array.new(8) {nil} }
     # @null_piece = Null_piece.new
   end
 
   def [](pos)
     row, col = pos
-    grid[row, col]
+    grid[row][col]
   end
 
   def []=(pos, value)
     row, col = pos
-    grid[row, col] = value
+    grid[row][col] = value
   end
 
   def move_piece(start_pos, end_pos)
-    raise "no" if self[start_pos] == nil || end_pos.all? { |ele| ele.between?(0, 8) }
+    raise "no" if !end_pos.all? { |ele| ele.between?(0, 8) } #[0, 2]
+    #self[start_pos] == nil || 
+    self[end_pos] = self[start_pos]
+    self[start_pos] = nil 
     # start pos = null piece
     #end pos = Piece
   end
