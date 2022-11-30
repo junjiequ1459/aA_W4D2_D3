fab1d1cc2b26c5d27e665c2a33d4842375e3f67a
rexqu <junjiequ123@gmail.com>
2022-11-30 10:05:57 -0500

    worked on chess

--- Chess/Piece.rb
+++ Chess/Piece.rb
@@ -1,7 +1,9 @@
 class Piece
   attr_accessor :board, :pos
 
-  def initialize(board, pos)
+  def initialize(symbol, color, board, pos)
+    @symbol = symbol
+    @color = color
     @board = board
     @pos = pos
   end
