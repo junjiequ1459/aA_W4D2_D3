31629e6bfe8a6c7ffe3d6da2439c10acec126987
rexqu <junjiequ123@gmail.com>
2022-11-30 10:05:51 -0500

    worked on chess

---
+++ Board.rb
@@ -0,0 +1,31 @@
+require_relative "Piece"
+
+class Board
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8, nil) }
+    # @null_piece = Null_piece.new
+  end
+
+  def [](pos)
+    row, col = pos
+    grid[row, col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    grid[row, col] = value
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise "no" if self[start_pos] == nil || end_pos.all? { |ele| ele.between?(0, 8) }
+    # start pos = null piece
+    #end pos = Piece
+
+  end
+
+  private
+
+  attr_accessor :null_piece
+end
--- Chess/modules.rb
+++ Chess/modules.rb
@@ -2,7 +2,25 @@ module Slideable
   HORIZONTAL_DIRS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
   DIAGONAL_DIRS = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
 
+  #logic determining horizontal or diagonal
+
+  #if horizontal
+
+  #rook
+  #result = []
+  #Horizontal.each do |ele|
+  #
+  #grow_unblocked_in_dir(ele)
+  #
   def moves
+    grow_unblocked_in_dir() # runs 4 times
+  end
+
+  def grow_unblocked_in_dir(dx, dy)
+
+    #until out of bounds
+    #keep adding 1 in the dy
+    # [0,1] => [[0,2],[0,3],[0,4]]
   end
 end
 
